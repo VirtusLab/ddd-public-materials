5ab05753ab9d7b8d5a70842c7802d2cbdaad7100
Jacek Dubikowski <jacek.dubikowski@gmail.com>
2020-05-17 14:41:51 +0200

    Add description to Property Based Equality task

---
+++ ddd-koans/Value Object/Property Based Equality/src/Main.kt
@@ -0,0 +1,12 @@
+class TimeSignature(val numerator: Int, val denominator: Int) {
+    init {
+        require(numerator in 1..32) {
+            "Numerator must be integer between 1 and 32"
+        }
+        require(denominator in 1..32 && denominator.isPowerOfTwo()) {
+            "Denominator must be integer that is power of two and between 1 and 32"
+        }
+    }
+
+    override fun toString(): String = "TimeSignature($numerator/$denominator)"
+}
