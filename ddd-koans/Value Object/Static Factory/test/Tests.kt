3022c08c902558b428d001acd4c2c859383f50d5
Jacek Dubikowski <jacek.dubikowski@gmail.com>
2020-05-25 22:46:28 +0200

    Extend static factory task with new method to implement

--- ddd-koans/Value Object/Static Factory/test/Tests.kt
+++ ddd-koans/Value Object/Static Factory/test/Tests.kt
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
@@ -23,6 +24,26 @@ class Test {
         )
     }
 
+    @ParameterizedTest
+    @MethodSource("generateValidTimeSignatures")
+    fun testIfOfFactoryMethodCreatesAllValidTimeSignaturesUsingAllValidStrings(arguments: Pair<Int, Int>) {
+        val (numerator, denominator) = arguments
+        assertEquals(
+            TimeSignature.of("$numerator/$denominator"),
+            TimeSignature.create(numerator, denominator)
+        )
+    }
+
+    @ParameterizedTest
+    @MethodSource("generateInvalidTimeSignatureStrings")
+    fun testIfOfFactoryMethodCreatesInvalidTimeSignaturesOnInvalidStrings(invalidTimeSignature: String) {
+        assertTrue(
+            TimeSignature.of(invalidTimeSignature) is InvalidTimeSignature,
+            "Creating TimeSignatures using values \"$invalidTimeSignature\" should return InvalidTimeSignature"
+        )
+    }
+
+
     companion object {
         @JvmStatic
         fun generateInvalidTimeSignatures(): List<Pair<Int, Int>> {
@@ -40,5 +61,24 @@ class Test {
 
         @JvmStatic
         fun generateValidTimeSignatures() = allValidValuesForTimeSignature()
+
+        @JvmStatic
+        fun generateInvalidTimeSignatureStrings(): List<String> {
+            val invalidTimeSignatureStrings = generateInvalidTimeSignatures().map { (n, d) -> "$n/$d" }
+            val empty = ""
+            val blank = "    "
+            val blankWithSlash = "  /  "
+            val zeroNominatorAndDenominator = "0/0"
+            val threeNumbers = "2/2/2"
+            val timeSignatureAndExtraWhitespace = " 1/12      "
+            val slashOnly = "/"
+            val timeSignatureUsingLetters = "a/b"
+            val timeSignatureUsingSpecialCharacters = "&/@"
+            val nominatorOnly = "1/"
+            val denominatorOnly = "/1"
+            return invalidTimeSignatureStrings + empty + blank + blankWithSlash + zeroNominatorAndDenominator +
+                    threeNumbers + timeSignatureAndExtraWhitespace + slashOnly + timeSignatureUsingLetters +
+                    nominatorOnly + denominatorOnly + timeSignatureUsingSpecialCharacters
+        }
     }
 }
