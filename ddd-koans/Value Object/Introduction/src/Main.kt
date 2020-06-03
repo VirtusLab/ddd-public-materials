0910521770d0c9c69953890cb1a7f4b181168f48
Konrad Jakubiec <Konrad.Jakubiec@uk.tesco.com>
2020-06-03 12:10:10 +0200

    fix typo in comment2

--- ddd-koans/Value Object/Introduction/src/Main.kt
+++ ddd-koans/Value Object/Introduction/src/Main.kt
@@ -1,3 +1,3 @@
 fun main() {
-    //Nothing to do here yet. Read the introduction part and feel free to move to the next exercies.
+    //Nothing to do here yet. Read the introduction part and feel free to move to the next exercise.
 }
