19f136d7eb1b913fa9fca7a4bc62a63d342b9e4d
Jacek Dubikowski <jacek.dubikowski@gmail.com>
2020-06-09 19:17:47 +0200

    Tiny types task, without description

---
+++ ddd-koans/Value Object/Tiny Types/src/Main.kt
@@ -0,0 +1,37 @@
+sealed class TimeSignature {
+    companion object {
+        private val timeSignatureRegex = "^(\\d{1,2})/(\\d{1,2})$".toRegex()
+
+        fun create(numerator: Int, denominator: Int): TimeSignature {
+            return runCatching {
+                ValidTimeSignature(Numerator(numerator), Denominator(denominator))
+            }.getOrDefault(InvalidTimeSignature)
+        }
+
+        fun of(timeSignature: String): TimeSignature {
+            val groupValues = timeSignatureRegex.matchEntire(timeSignature)?.groupValues
+            return if (groupValues == null) {
+                InvalidTimeSignature
+            } else {
+                val (numerator, denominator) = (groupValues[1] to groupValues[2])
+                create(numerator.toInt(), denominator.toInt())
+            }
+        }
+    }
+}
+
+object InvalidTimeSignature : TimeSignature()
+
+data class ValidTimeSignature(private val numerator: Numerator, private val denominator: Denominator) : TimeSignature()
+
+data class Numerator(val value: Int) {
+    init {
+        require(value in 1..32)
+    }
+}
+
+data class Denominator(val value: Int) {
+    init {
+        require(value in 1..32 && value.isPowerOfTwo())
+    }
+}
