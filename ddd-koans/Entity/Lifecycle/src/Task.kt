8a94038f7615ec454a37a22c108982abefc30893
Jacek Dubikowski <jacek.dubikowski@gmail.com>
2021-04-24 17:27:49 +0200

    lifecycle task structure

--- ddd-koans/Entity/Lifecycle/src/Task.kt
+++ ddd-koans/Entity/Lifecycle/src/Task.kt
@@ -3,13 +3,21 @@ class Bar(
     private val notes: Notes,
     private val timeSignature: ValidTimeSignature
 ) {
+    init {
+        validateState()
+    }
+
+    fun state(): State {
+        TODO()
+    }
+
     fun id(): Ordinal = ordinal
 
     fun equal(other: Bar): Boolean {
         return id() == other.id()
     }
 
-    override fun equals(other: Any?): Boolean = when(other) {
+    override fun equals(other: Any?): Boolean = when (other) {
         null -> false
         is Bar -> equal(other)
         else -> false
@@ -20,6 +28,14 @@ class Bar(
     override fun toString(): String {
         return "Bar(ordinalNumber=$ordinal, notes=$notes, timeSignature=$timeSignature)"
     }
+
+    private fun validateState() {
+        state()
+    }
+
+    enum class State {
+        Complete, Incomplete
+    }
 }
 
 data class Ordinal(val value: Int) {
@@ -27,3 +43,6 @@ data class Ordinal(val value: Int) {
         require(value > 0) { "ordinal must be positive integer" }
     }
 }
+
+class BarWithTooManyNotesException :
+    RuntimeException("Bar has too many notes and sum of beats is bigger than declared in the time signature.")
